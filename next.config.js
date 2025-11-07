de1f5dcb948dbc0ddf308834e91facca9ded9e36
Kalpit <kalpitchandekar1736@gmail.com>
2025-11-07 11:31:30 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // enables static export for Amplify
   images: {
+    unoptimized: true, // required for static hosting (no server-side optimization)
     domains: ["images.unsplash.com"], // allow Unsplash images
   },
 };
